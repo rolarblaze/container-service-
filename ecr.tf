23020bddcb1ad4d91e0c4f3968ac26972c84fd28
rolarblaze <wordofwisdom_2@yahoo.com>
2026-02-23 14:39:04 +0100

    ECR git commit

--- ecr.tf
+++ ecr.tf
@@ -1,6 +1,11 @@
 resource "aws_ecr_repository" "app_repo" {
   name                 = "app-repo"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
   tags = {
     Name = "app-portal"
